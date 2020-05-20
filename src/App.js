f23195d7232c29ad5998fc8def1a78794e0e637f
franciscohui <franciscohui@users.noreply.github.com>
2020-05-20 12:32:53 -0400

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Projects from "./Projects";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>React Project Ideas</h1>
+      <p>A list of projects I'd like to build in React</p>
+      <Projects />
+    </div>
+  );
+}
+
+/*
+Simple react apps
+
+Component List of project ideas
+> manually create the list
+> pull in from an open API (google sheets JSON)
+
+Timer app
+Start timer
+When it stops, open a page
+Pause and resume timer
+
+Timer app stories: 
+- Render time
+- Update time
+- 
+
+
+
+
+
+*/
---
+++ src/Projects.js
@@ -0,0 +1,30 @@
+import React from "react";
+import "./styles.css";
+
+const SingleProject = myProps => (
+  <div className="SingleProject">
+    <div className="ProjectTitle">{myProps.title}</div>
+    <div className="ProjectSummary">{myProps.summary}</div>
+    <div className="ProjectDetail">{myProps.detail}</div>
+  </div>
+);
+
+export default function Projects() {
+  return (
+    <div className="ProjectList">
+      <SingleProject
+        title="Project List"
+        summary="A list of projects with titles, summaries, and a done state."
+        detail="Bonus: Populate projects via Google Sheets or AirTable. Initial commit to github"
+      />
+      <SingleProject
+        title="Timer"
+        summary="Start, stop; pause, resume, and set time"
+      />
+      <SingleProject
+        title="Color Picker"
+        summary="Set primary color, toggle complementaries, add secondary, add saturation variations"
+      />
+    </div>
+  );
+}
